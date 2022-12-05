3fa07c0e156f8727c40f6643d0748c43a9e49579
peak <peak@jiemogame.com>
2022-12-05 15:33:29 +0800

    update resign.sh

--- resign-ipa/resign.sh
+++ resign-ipa/resign.sh
@@ -2,65 +2,71 @@
 
 set -eu
 
-provisionFileName="gods_strike_dev"
+RED="\033[31m"
+BLUE="\033[34m"
+NC="\033[0m" # no color
+
+provisionFileName="xxxx"
 
 ipaPath=$(find $(pwd) -name *.ipa) || {
-	echo "[$(pwd)] must only exist one ipa file, please checkout."
-	exit 1
+    echo "[$(pwd)] must only exist one ipa file, please checkout."
+    exit 1
 }
 workDir=$(dirname $ipaPath)
-outputIpaPath="${workDir}/resign.ipa"
+ipaName=${ipaPath##*/} # delete the last '/' 
+ipaName="${ipaName%.*}_resign.ipa" # delete last '.' and append '_resign.ipa'
+outputIpaPath="${workDir}/${ipaName}"
 entitlementPlistPath="${workDir}/entitlements.plist"
 
 function clean() {
-	if [[ -d "${workDir}/Payload" ]]; then
-		rm -rf "${workDir}/Payload"
-	fi
-	if [[ -f "${entitlementPlistPath}" ]]; then
-		rm -rf "${entitlementPlistPath}"
-	fi
+    if [[ -d "${workDir}/Payload" ]]; then
+	rm -rf "${workDir}/Payload"
+    fi
+    if [[ -f "${entitlementPlistPath}" ]]; then
+        rm -rf "${entitlementPlistPath}"
+    fi
 }
 
 function log() {
-	echo "$@"
+    echo "$@"
 }
 
 function quit() {
-	echo "$@"
-	clean
-	exit 1
+    echo "$@"
+    clean
+    exit 1
 }
 
 log "working in: ${workDir}"
-log "unzip ${ipaPath} ..."
+log "unzip: ${BLUE}${ipaPath}${NC}"
 clean && unzip -q "${ipaPath}" || quit "unzip ipa fail"
 
 appPath=$(find ${workDir} -name *.app -type d)
-log "app file: ${appPath}"
+log "app file: ${BLUE}${appPath}${NC}"
 
 # bussiness
-jsonPath=$(find ${appPath} -name parameter.json)
-if [[ -f $jsonPath ]]; then
-	log "modify $jsonPath"
-	sed -i ' ' "s/ios-pay.godsstrike.com/pre-test-aws-kmpay.karmasgame.com/g" $jsonPath
-fi
+# jsonPath=$(find ${appPath} -name parameter.json)
+# if [[ -f $jsonPath ]]; then
+#     log "modify $jsonPath"
+#     sed -i ' ' "s/ios-pay.godsstrike.com/pre-test-aws-kmpay.karmasgame.com/g" $jsonPath
+# fi
 
 provisionFilePath=""
 provisionFileDir="$HOME/Library/MobileDevice/Provisioning Profiles"
 for file in $(ls -Ut "${provisionFileDir}"); do
-	name=$(/usr/libexec/PlistBuddy -c 'Print :Name' /dev/stdin <<< $(/usr/bin/security cms -D -i "${provisionFileDir}/$file"))
-	if [[ ${name} == "${provisionFileName}" ]]; then
-		provisionFilePath="${provisionFileDir}/${file}"
-		log "provision file path: $provisionFilePath"
-		break
-	fi
+    name=$(/usr/libexec/PlistBuddy -c 'Print :Name' /dev/stdin <<< $(/usr/bin/security cms -D -i "${provisionFileDir}/$file"))
+    if [[ ${name} == "${provisionFileName}" ]]; then
+	provisionFilePath="${provisionFileDir}/${file}"
+	log "provision file path: $provisionFilePath"
+	break
+    fi
 done
 if [[ -z ${provisionFilePath} ]]; then
-	quit "not exist provision file: ${provisionFileName}"
+    quit "not exist provision file: ${provisionFileName}"
 fi
 embededProvisionFile="${appPath}/embedded.mobileprovision"
 if [[ -f ${embededProvisionFile} ]]; then
-	rm -rf ${embededProvisionFile}
+    rm -rf ${embededProvisionFile}
 fi
 cp "${provisionFilePath}" "${embededProvisionFile}"
 
@@ -89,20 +95,26 @@ resignPaths=$(find "$appPath" -d -name *.app \
 							  -o -name *.egg -type f \
 							  -o -name *.0 -type f)
 for path in $resignPaths; do
-	log "[resign] /usr/bin/codesign -vvv -fs \"${codeSignValue}\" --no-strict --entitlements \"${entitlementPlistPath}\" \"${path}\""
-	/usr/bin/codesign -vvv -fs "${codeSignValue}" --no-strict --entitlements "${entitlementPlistPath}" "${path}"
-	log ""
+    log "[resign] ${RED}/usr/bin/codesign -vvv -fs \"${codeSignValue}\" --no-strict --entitlements \"${entitlementPlistPath}\" \"${path}\" ${NC}"
+    /usr/bin/codesign -vvv -fs "${codeSignValue}" --no-strict --entitlements "${entitlementPlistPath}" "${path}"
+    echo ""
 done
 
+echo ""
 log "verify ipa ..."
 /usr/bin/codesign --verify "${appPath}" || quit "verify ipa fail"
 log "verify success"
 
+echo ""
 payloadPath=$(dirname "${appPath}")
 if [[ -f "${outputIpaPath}" ]]; then
-	rm -rf "${outputIpaPath}"
+    rm -rf "${outputIpaPath}"
 fi
-log "zip ${payloadPath} -> ${outputIpaPath} ..."
+log "zip: \n${BLUE}${payloadPath}${NC}\n->\n${BLUE}${outputIpaPath}${NC}"
 (cd $(dirname "${payloadPath}") && zip -qyr "${outputIpaPath}" * && (cd "${workDir}")) || quit "zip ipa fail"
 log "zip success"
+
+echo ""
+clean
+log "clear success"
 
