a0f7fbaf470166aa72db76838235bb208ad9a5dd
peak <peak@jiemogame.com>
2024-03-29 10:20:33 +0800

    update ProvisionFileManager

--- Unity/ProvisionFileManager.cs
+++ Unity/ProvisionFileManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 using Debug = UnityEngine.Debug;
@@ -68,14 +69,43 @@ public static class ProvisionFileManager
         /// <summary>
         /// 证书名称
         /// </summary>
-        public string CertificateName;
+        public string CertificateName
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(AppleCertificateName))
+                {
+                    return AppleCertificateName;
+                } 
+                else 
+                {
+                    return iPhoneCertificateName;
+                }
+            }
+        }
+
+        public string iPhoneCertificateName;
+        public string AppleCertificateName;
 
         public void Description()
         {
-            string description = string.Format("\n=============={0}==============\n" +
-                "BundleIdentifier: {1}\nUDID: {2}\nTeamIdentifier: {3}\nCertificateName: {4}\nFilePath: {5}\n",
-                Name, BundleIdentifier, UDID, TeamIdentifier, CertificateName, FilePath);
-            Debug.Log(description);
+            Debug.Log(this.ToString());
+        }
+
+        override public string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"Name: {Name} \n");
+            builder.Append($"Development: {Development} \n");
+            builder.Append($"BundleId: {BundleIdentifier} \n");
+            builder.Append($"TeamIdentifier: {TeamIdentifier} \n");
+            builder.Append($"CertificateName: {CertificateName} \n");
+            builder.Append($"iPhoneCertificateName: {iPhoneCertificateName} \n");
+            builder.Append($"AppleCertificateName: {AppleCertificateName} \n");
+            builder.Append($"UDID: {UDID} \n");
+            builder.Append($"ExpirationDate: {ExpirationDate} \n");
+            builder.Append($"FilePath: {FilePath} \n");
+            return builder.ToString();
         }
     }
 
@@ -92,13 +122,15 @@ public static class ProvisionFileManager
         List<ProvisionFileInfo> provisionFileInfos = new List<ProvisionFileInfo>();
         foreach (var filePath in profilePaths)
         {
-            provisionFileInfos.Add(ParseFile(filePath));
+            ProvisionFileInfo fileInfo = ParseFile(filePath);
+            provisionFileInfos.Add(fileInfo);
         }
 
         List<ProvisionFileInfo> filterProvisionFileInfo = provisionFileInfos
-            .Where(p => p.BundleIdentifier == bundleIdentifier).ToList()
-            .Where(p => p.Development == isDevelopment).ToList()
-            .OrderByDescending(p => p.ExpirationDateTimeStamp).ToList();
+            .Where(p => p.BundleIdentifier == bundleIdentifier)
+            .Where(p => p.Development == isDevelopment)
+            .OrderByDescending(p => p.ExpirationDateTimeStamp)
+            .ToList();
 
         if (filterProvisionFileInfo.Count > 0)
         {
@@ -110,81 +142,135 @@ public static class ProvisionFileManager
 #endif
     }
 
-    private static ProvisionFileInfo ParseFile(string filePath)
+    public static ProvisionFileInfo ParseFile(string filePath)
     {
         ProvisionFileInfo fileInfo = new ProvisionFileInfo
         {
             FilePath = filePath
         };
 
-        var provisioningFileContents = File.ReadAllText(filePath);
-
-        string patternUUID = "<key>UUID<\\/key>[\n\t]*<string>((\\w*\\-?){5})";
-        Match matchUDID = Regex.Match(provisioningFileContents, patternUUID, RegexOptions.Singleline);
-        if (matchUDID.Success)
+        try
         {
-            string udid = matchUDID.Groups[1].Value;
-            fileInfo.UDID = udid;
-        }
+            var provisioningFileContents = File.ReadAllText(filePath);
 
-        string patternName = "<key>Name<\\/key>[\n\t]*<string>((\\w*\\-?){5})";
-        Match matchName = Regex.Match(provisioningFileContents, patternName, RegexOptions.Singleline);
-        if (matchName.Success)
-        {
-            string name = matchName.Groups[1].Value;
-            fileInfo.Name = name;
-        }
-
-        string patternTeamIdentifier = "<key>TeamIdentifier<\\/key>[\n\t]*<array>[\n\t]*<string>([\\w\\/+=]+)<\\/string>";
-        Match matchTeamIdentifier = Regex.Match(provisioningFileContents, patternTeamIdentifier, RegexOptions.Singleline);
-        if (matchTeamIdentifier.Success)
-        {
-            string teamIdentifier = matchTeamIdentifier.Groups[1].Value;
-            fileInfo.TeamIdentifier = teamIdentifier;
-        }
-
-        string patternExpirationDate = "<key>ExpirationDate<\\/key>[\n\t]*<date>(.*?)</date>";
-        Match matchExpirationDate = Regex.Match(provisioningFileContents, patternExpirationDate, RegexOptions.Singleline);
-        if (matchExpirationDate.Success)
-        {
-            string expirationDate = matchExpirationDate.Groups[1].Value;
-            System.DateTime dateTime = System.DateTime.Parse(expirationDate);
-
-            TimeSpan span = (dateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
-            fileInfo.ExpirationDateTimeStamp = (double)span.TotalSeconds;
-
-            expirationDate = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
-            fileInfo.ExpirationDate = expirationDate;
-        }
-
-        string patternBundleIdentifier = "<key>application-identifier<\\/key>[\n\t]*<string>(.*?)</string>";
-        Match matchBundeIdentifier = Regex.Match(provisioningFileContents, patternBundleIdentifier, RegexOptions.Singleline);
-        if (matchBundeIdentifier.Success)
-        {
-            string bundleIdentifier = matchBundeIdentifier.Groups[1].Value;
-            string bundleId = string.Join(".", bundleIdentifier.Split('.').Skip(1).ToArray());
-            fileInfo.BundleIdentifier = bundleId;
-        }
-
-        string plistCmd = "/usr/libexec/PlistBuddy -c 'Print :DeveloperCertificates:0' /dev/stdin";
-        string opensslCmd = "openssl x509 -inform DER -noout -subject";
-        string sedCmd = "sed -n '/^subject/s/^.*CN=\\(.*\\)\\/OU=.*/\\1/p'";
-        string cmd = string.Format("{0} <<< $(security cms -D -i {1}) | {2} | {3}",
-           plistCmd, filePath.Replace(" ", "\\ "), opensslCmd, sedCmd);
-        string signInfo = RunCommand(cmd);
-        if (signInfo != null)
-        {
-            signInfo = signInfo.Replace("\n", "");
-            if (signInfo.Contains("/x"))
+            string patternUUID = "<key>UUID<\\/key>[\n\t]*<string>((\\w*\\-?){5})";
+            Match matchUDID = Regex.Match(provisioningFileContents, patternUUID, RegexOptions.Singleline);
+            if (matchUDID.Success)
             {
-                // 中文: 转成UTF8格式， 添加%, 使用URLDecode来解码
-                signInfo = signInfo.Replace("/x", "%");
-                signInfo = Uri.UnescapeDataString(signInfo);
+                string udid = matchUDID.Groups[1].Value;
+                fileInfo.UDID = udid;
             }
-            fileInfo.CertificateName = signInfo;
-            fileInfo.Development = signInfo.StartsWith("iPhone Developer:");
+
+            string patternTeamIdentifier = "<key>TeamIdentifier<\\/key>[\n\t]*<array>[\n\t]*<string>([\\w\\/+=]+)<\\/string>";
+            Match matchTeamIdentifier = Regex.Match(provisioningFileContents, patternTeamIdentifier, RegexOptions.Singleline);
+            if (matchTeamIdentifier.Success)
+            {
+                string teamIdentifier = matchTeamIdentifier.Groups[1].Value;
+                fileInfo.TeamIdentifier = teamIdentifier;
+            }
+
+            string patternExpirationDate = "<key>ExpirationDate<\\/key>[\n\t]*<date>(.*?)</date>";
+            Match matchExpirationDate = Regex.Match(provisioningFileContents, patternExpirationDate, RegexOptions.Singleline);
+            if (matchExpirationDate.Success)
+            {
+                string expirationDate = matchExpirationDate.Groups[1].Value;
+                System.DateTime dateTime = System.DateTime.Parse(expirationDate);
+
+                TimeSpan span = (dateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+                fileInfo.ExpirationDateTimeStamp = (double)span.TotalSeconds;
+
+                expirationDate = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+                fileInfo.ExpirationDate = expirationDate;
+            }
+
+            string patternBundleIdentifier = "<key>application-identifier<\\/key>[\n\t]*<string>(.*?)</string>";
+            Match matchBundeIdentifier = Regex.Match(provisioningFileContents, patternBundleIdentifier, RegexOptions.Singleline);
+            if (matchBundeIdentifier.Success)
+            {
+                string bundleIdentifier = matchBundeIdentifier.Groups[1].Value;
+                string bundleId = string.Join(".", bundleIdentifier.Split('.').Skip(1).ToArray());
+                fileInfo.BundleIdentifier = bundleId;
+            }
+
+            filePath = filePath.Replace(" ", "\\ ");
+
+            string nameCmd = $"/usr/libexec/PlistBuddy -c 'Print :Name' /dev/stdin <<< $(security cms -D -i {filePath})";
+            string name = RunCommand(nameCmd);
+            if (!string.IsNullOrEmpty(name))
+            {
+                fileInfo.Name = name;
+            }
+
+            HandleSignInfo(filePath, 0, fileInfo);
+            HandleSignInfo(filePath, 1, fileInfo);
+
+            string apsEnvironmentCmd = $"/usr/libexec/PlistBuddy -c 'Print :Entitlements:aps-environment' /dev/stdin <<< $(security cms -D -i {filePath})";
+            string apsEnvironment = RunCommand(apsEnvironmentCmd);
+            if (!string.IsNullOrEmpty(apsEnvironment))
+            {
+                fileInfo.Development = apsEnvironment == "development";
+            }
+            else 
+            {
+                if (!string.IsNullOrEmpty(fileInfo.CertificateName))
+                {
+                    fileInfo.Development = fileInfo.CertificateName.StartsWith("iPhone Developer");
+                } else if (!string.IsNullOrEmpty(fileInfo.AppleCertificateName))
+                {
+                    fileInfo.Development = fileInfo.CertificateName.StartsWith("Apple Development");
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"{filePath} occur exception: {e}");
         }
         return fileInfo;
+    }
+
+    private static void HandleSignInfo(string filePath, int certificateIndex, ProvisionFileInfo fileInfo)
+    {
+        string signInfo = ExtractSignInfo(filePath, certificateIndex);
+        if (!string.IsNullOrEmpty(signInfo))
+        {
+            if (signInfo.StartsWith("iPhone"))
+            {
+                fileInfo.iPhoneCertificateName = signInfo;
+            }
+            else if (signInfo.StartsWith("Apple"))
+            {
+                fileInfo.AppleCertificateName = signInfo;
+            }
+        }
+    }
+
+    private static string ExtractSignInfo(string filePath, int certificateIndex = 0)
+    {
+        // example data:
+        // subject= /UID=2Z2VV22UHQ/CN=iPhone Developer: zeze huang (6YD3597U59)/OU=9XJ9HHK8NJ/O=KARMAGAME HK LIMITED/C=HK
+        // subject= /UID=Q7YXYXBXMK/CN=Apple Development: bdNnew peak (5QJ63R923J)/OU=9XJ9HHK8NJ/O=KARMAGAME HK LIMITED/C=HK
+        string cmd = $"/usr/libexec/PlistBuddy -c 'Print :DeveloperCertificates:{certificateIndex}' /dev/stdin <<< $(security cms -D -i {filePath}) | openssl x509 -inform DER -noout -subject";
+        string signInfo = RunCommand(cmd);
+        if (!string.IsNullOrEmpty(signInfo))
+        {
+            string pattern = @"CN=(.*?)/OU";
+            var match = Regex.Match(signInfo, pattern);
+            if (match.Success)
+            {
+                string result = match.Groups[1].Value;
+                if (result.Contains("/x"))
+                {
+                    // 中文: 转成UTF8格式， 添加%, 使用URLDecode来解码
+                    result = result.Replace("/x", "%");
+                    return Uri.UnescapeDataString(result);
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+        return "";
     }
 
     private static List<string> LoadLocalProfiles()
@@ -234,20 +320,23 @@ public static class ProvisionFileManager
         };
         process.Start();
 
-        process.ErrorDataReceived += delegate (object sender, DataReceivedEventArgs args) {
+        process.ErrorDataReceived += delegate (object sender, DataReceivedEventArgs args)
+        {
             Debug.LogError(args.Data);
         };
-        process.OutputDataReceived += delegate (object sender, DataReceivedEventArgs args) {
+        process.OutputDataReceived += delegate (object sender, DataReceivedEventArgs args)
+        {
             Debug.LogError(args.Data);
         };
-        process.Exited += delegate (object sender, System.EventArgs args) {
+        process.Exited += delegate (object sender, System.EventArgs args)
+        {
             Debug.LogError(args.ToString());
         };
 
         string line = process.StandardOutput.ReadToEnd();
         if (line != null)
         {
-            return line.Replace("\\", "/");
+            return line.Replace("\\", "/").Replace("\n", "");
         }
 
         string error = process.StandardError.ReadToEnd();
